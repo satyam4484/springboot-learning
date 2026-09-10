619446495987f824d700eab28d756840ad861c93
satyam4484 <satyambsingh93@gmail.com>
2026-09-10 22:14:19 +0530

    p1-configuration

--- src/main/java/com/ecommerce/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/ecommerce/EcommerceApplication.java
@@ -2,55 +2,11 @@ package com.ecommerce.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import com.ecommerce.ecommerce.learning.component_.UserController;
 
 @SpringBootApplication
 public class EcommerceApplication {
 
-	public static void main(String[] args) {
-		
-		ConfigurableApplicationContext context = SpringApplication.run(EcommerceApplication.class, args);
-		UserController userController = context.getBean(UserController.class);
-		userController.greetUser("Satyam");
-	}
-
+        public static void main(String[] args) {
+                SpringApplication.run(EcommerceApplication.class, args);
+        }
 }
-
-        //          🚀 Spring Boot starts
-        //                  │
-        //                  ↓
-        //       @SpringBootApplication
-        //                  │
-        //                  ↓
-        //          Component Scanning
-        //                  │
-        //                  ↓
-        //       Finds @Component classes
-        //                  │
-        //                  ↓
-        //         ┌─────────────────┐
-        //         │ GreetingService │
-        //         │   @Component    │
-        //         └────────┬────────┘
-        //                  │
-        //                  ↓
-        //       Spring creates object
-        //                  │
-        //                  ↓
-        //           Spring Bean
-        //                  │
-        //                  ↓
-        //       Spring Container
-        //       (ApplicationContext)
-        //                  │
-        //                  ↓
-        //   ┌─────────────────────────┐
-        //   │  GreetingController     │
-        //   │                         │
-        //   │ GreetingService service │
-        //   └────────────┬────────────┘
-        //                ↑
-        //                │
-        //          Spring injects it
