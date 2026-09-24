8810c8e3321a1ca08960c956d6cb1c071f8d1561
satyam4484 <satyambsingh93@gmail.com>
2026-09-24 12:27:32 +0530

    Add Redis caching support and configuration for improved performance

--- projects/ecommerce/ecommerce/src/main/java/com/ecom/ecommerce/EcommerceApplication.java
+++ projects/ecommerce/ecommerce/src/main/java/com/ecom/ecommerce/EcommerceApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // import caching
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class EcommerceApplication {
 
   public static void main(String[] args) {
---
+++ projects/ecommerce/ecommerce/src/main/java/com/ecom/ecommerce/config/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.ecom.ecommerce.config;
+
+import java.time.Duration;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Bean
+    public CacheManager cacheManager(
+            RedisConnectionFactory connectionFactory) {
+
+        JacksonJsonRedisSerializer<Object> serializer =
+                new JacksonJsonRedisSerializer<>(Object.class);
+
+        RedisCacheConfiguration cacheConfiguration =
+                RedisCacheConfiguration.defaultCacheConfig()
+                        .entryTtl(Duration.ofMinutes(10))
+                        .serializeValuesWith(
+                                RedisSerializationContext.SerializationPair
+                                        .fromSerializer(serializer)
+                        );
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
+    }
+}
